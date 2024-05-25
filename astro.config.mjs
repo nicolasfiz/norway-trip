af542d88b9e1c98f3c4a5706a33fc6e3af5a25be
nicolasfizDNV <nicolas.martin.benchetrit@dnv.com>
2024-05-25 18:31:38 +0200

    change name

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://nicolasfiz.github.io',
-  base: '/norway-trip'
+  base: '/surprise'
 });
